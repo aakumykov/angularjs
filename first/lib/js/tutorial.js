1b2a5d2419a213e71d5d80b53303de7048946399
Андрей Кумыков <aakumykov@yandex.ru>
2016-08-19 18:05:26 +0700

    Добавляю форму добавления.

--- first/lib/js/tutorial.js
+++ first/lib/js/tutorial.js
@@ -1,13 +1,20 @@
 var app = angular.module("myShoppingList", ["ngRoute"]);
 
-app.controller("myCtrl", function($scope) {
+app.controller("myCtrl", function($scope, $http) {
 	$scope.products = ["Debian", "Mandriva", "Ubuntu"];
 	$scope.msgText = "";
 	$scope.msgColor = "";
 
+	$scope.qwerty = 'qwerty из контроллера'
+	$scope.qwerty2 = 'qwerty2 из контроллера'
+
 	$scope.addItem = function(){
+		//alert($scope.newDistro);
 		var d = $scope.newDistro;
-		if (!d) { return; }
+		if (!d) { 
+			alert('ничё нет');
+			return; 
+		}
 		if (-1 == $scope.products.indexOf(d) ) {
 			$scope.products.push(d);
 			$scope.msgText = "Элемент " + d + " добавлен"
@@ -30,26 +37,38 @@ app.controller("myCtrl", function($scope) {
 			$scope.msgColor = "red";
 		}
 	}
+
+	$scope.postNewEntry = function(){
+		alert($scope);
+		// $http.post('127.0.0.1', {
+		// 	title: $scope.newEntryTitle,
+		// 	body: newEntryBody
+		// });
+	};
+
+	$scope.testQwerty = function(){
+		alert('testQwerty: '+$scope.qwerty);
+	};
 });
 
 app.config(function($routeProvider){
 	$routeProvider
 	.when("/",{
 		templateUrl: 'main.html',
-		controller: 'mainCtrl'
+		controller: 'myCtrl'
 	})
 	.when("/add",{
-		templateUrl: 'add.html'
+		templateUrl: 'add.html',
+		controller: 'myCtrl'
 	})
 	.when("/list",{
 		templateUrl: "list.html",
 		controller: 'myCtrl'
 	})
 	.otherwise({
-		templateUrl: "main.html"
+		templateUrl: "main.html",
+		controller: 'myCtrl'
 	});
 });
 
-app.controller('mainCtrl', function($scope){
-	$scope.title = 'Глагне';
-});
+
